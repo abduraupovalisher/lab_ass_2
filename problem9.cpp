7aa530e3ea98e368e8e60f4b66499e4baca45737
abduraupovalisher <102685756+abduraupovalisher@users.noreply.github.com>
2024-03-13 16:40:31 +0500

    Update problem9.cpp

--- problem9.cpp
+++ problem9.cpp
@@ -3,36 +3,6 @@
 #include <algorithm>
 using namespace std;
 
-struct Student {
-    int id;
-    int totalGrade;
-};
-
-bool compareStudents(const Student& s1, const Student& s2) {
-    if (s1.totalGrade == s2.totalGrade) {
-        return s1.id < s2.id;
-    }
-    return s1.totalGrade > s2.totalGrade;
-}
-
-int findRank(const vector<vector<int>>& grades, int studentID) {
-    vector<Student> students;
-    for (int i = 0; i < grades.size(); ++i) {
-        int total = grades[i][0] + grades[i][1] + grades[i][2];
-        students.push_back({i + 1, total});
-    }
-
-    sort(students.begin(), students.end(), compareStudents);
-
-    for (int i = 0; i < students.size(); ++i) {
-        if (students[i].id == studentID) {
-            return i + 1;
-        }
-    }
-
-    return -1;
-}
-
 int main() {
     int N;
     cout << "Enter the number of students: ";
@@ -45,16 +15,28 @@ int main() {
         cin >> grades[i][0] >> grades[i][1] >> grades[i][2];
     }
 
+    vector<pair<int, int>> total_grades;
+
+    for (int i = 0; i < N; ++i) {
+        total_grades.push_back({grades[i][0] + grades[i][1] + grades[i][2], i + 1});
+    }
+
+    sort(total_grades.begin(), total_grades.end(), greater<pair<int, int>>());
+
     int studentID;
     cout << "Enter the ID of the student to find the rank: ";
     cin >> studentID;
 
-    int rank = findRank(grades, studentID);
-    if (rank != -1) {
-        cout << "Rank of Student with ID " << studentID << ": " << rank << endl;
-    } else {
-        cout << "Student with ID " << studentID << " not found." << endl;
+    int rank = 1;
+    for (int i = 0; i < N; ++i) {
+        if (total_grades[i].second == studentID) {
+            cout << "Rank of Student with ID " << studentID << ": " << rank << endl;
+            return 0;
+        }
+        rank++;
     }
+
+    cout << "Student with ID " << studentID << " not found." << endl;
 
     return 0;
 }
